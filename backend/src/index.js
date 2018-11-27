cbc8b20c35b4cb0ae8027864055863e93c74704e
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-11-27 14:19:12 -0300

    Fixing Backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,10 +13,10 @@ mongoose.connect(UrlMongo, { useNewUrlParser: true })
 
 app.use((req, res, next) => {
     req.io = io
-    return next
+    return next()
 })
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
